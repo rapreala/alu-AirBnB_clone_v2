fc64470e9d5e644dade5048ed17d849313e29d54
Ryan Apreala <r.apreala@alustudent.com>
2023-06-13 16:52:30 +0000

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 """This module instantiates an object of class DBStorage"""
 from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 from os import getenv
 
+# Import all models
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 storage_type = getenv('HBNB_STORAGE')
+
 if storage_type == 'db':
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 storage.reload()
