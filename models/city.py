f6e997b7c6efdfed9b96e1e8b66fcbc56db0202d
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-06-14 14:53:03 +0200

    DB-storage of all models

--- models/city.py
+++ models/city.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""City Module for HBNB project"""
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """The city class, contains state ID and name"""
-    __tablename__ = 'cities'
-
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    """ The city class, contains state ID and name """
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-
-    places = relationship("Place", backref="city", cascade="all, delete-orphan")
-
-    def __init__(self, *args, **kwargs):
-        """Initializes a new City instance"""
-        super().__init__(*args, **kwargs)
+    places = relationship('Place', backref='cities', cascade="delete")
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
