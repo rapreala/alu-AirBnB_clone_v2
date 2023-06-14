27a85bd123afb379c2574eb289e12fcd3d3edfe8
Ryan Apreala <r.apreala@alustudent.com>
2023-06-14 15:53:24 +0000

    fix

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,40 +1,56 @@
 #!/usr/bin/python3
-""" """
+"""
+Contains the TestUser class
+"""
+
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
+class TestUser(test_basemodel):
+    """
+    TestUser class to test the User class
+    """
 
-class test_User(test_basemodel):
-    """ """
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a TestUser object
+        """
+        super().__init__(*args, **kwargs)
+        self.name = "User"
+        self.value = User()
 
     def test_str(self):
-        """ """
-        new = self.value()
+        """
+        Test the __str__ method of User
+        """
+        new = self.value
         expected_str = '[User] ({}) {}'.format(new.id, new.__dict__)
         self.assertEqual(str(new), expected_str)
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
-
     def test_first_name(self):
-        """ """
-        new = self.value()
+        """
+        Test the first_name attribute of User
+        """
+        new = self.value
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
-        new = self.value()
+        """
+        Test the last_name attribute of User
+        """
+        new = self.value
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
-        new = self.value()
+        """
+        Test the email attribute of User
+        """
+        new = self.value
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
-        new = self.value()
+        """
+        Test the password attribute of User
+        """
+        new = self.value
         self.assertEqual(type(new.password), str)
