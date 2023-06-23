ea03e756e97729e358aaa2a5fc6b8f2472a65355
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-06-23 11:57:17 +0200

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,33 @@
 #!/usr/bin/python3
-# Fabric script that distributes an archive to your web servers.
+"""Fabric script that distributes an archive to your web servers"""
 from fabric.api import env, put, run
 from os.path import exists
-from datetime import datetime
 
+env.hosts = ["54.226.54.247", "18.205.246.150"]
+env.user = "ubuntu"
+env.key = "~/.ssh/id_rsa"
 
-env.hosts = ['54.209.26.141', '18.215.182.32']
-env.user = 'ubuntu'
-env.key_filename = '~/larissa/.ssh/id_rsa'
 
 def do_deploy(archive_path):
-    """Distribute an archive to the web servers."""
+    """Function to distribute an archive to your web servers"""
     if not exists(archive_path):
         return False
-
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp/')
-
-        # Extract the archive to the appropriate folder
-        archive_filename = archive_path.split('/')[-1]
-        release_folder = '/data/web_static/releases/{}'.format(archive_filename[:-4])
-        run('mkdir -p {}'.format(release_folder))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_folder))
-
-        # Delete the archive from the web server
-        run('rm /tmp/{}'.format(archive_filename))
-
-        # Move the contents of the extracted folder to the parent directory
-        run('mv {}/web_static/* {}/'.format(release_folder, release_folder))
-        run('rm -rf {}/web_static'.format(release_folder))
-
-        # Delete the symbolic link /data/web_static/current
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split(".")[0]
+        path_name = "/data/web_static/releases/" + name
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(path_name))
+        run('tar -xzf /tmp/{} -C {}/'.format(file_name, path_name))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}/web_static/* {}".format(path_name, path_name))
+        run("rm -rf {}/web_static".format(path_name))
         run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link
-        run('ln -s {} /data/web_static/current'.format(release_folder))
-
-        print("New version deployed!")
+        run('ln -s {}/ /data/web_static/current'.format(path_name))
         return True
-    except Exception as e:
-        print("Error: {}".format(e))
+    except Exception:
         return False
+
+# Run the script like this:
+# $ fab -f 2-do_deploy_web_static.py
+# do_deploy:archive_path=versions/file_name.tgz
